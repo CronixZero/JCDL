a935bcf78bc63228df0d697dff791ab90e12ebae
CronixZero <CronixZero@users.noreply.github.com>
2021-11-08 21:20:11 +0100

    Add Discord Bot Driver without dependencies

---
+++ discord-botdriver/src/main/java/xyz/cronixzero/sapota/botdriver/BotDescriptionTypeAdapter.java
@@ -0,0 +1,83 @@
+/* 
+Coded for Notazo
+Made by CronixZero
+Created 19.10.2021 - 00:28
+ */
+
+package xyz.cronixzero.sapota.botdriver;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.util.Locale;
+
+public class BotDescriptionTypeAdapter extends TypeAdapter<BotDescription> {
+
+    @Override
+    public void write(JsonWriter out, BotDescription value) throws IOException {
+        out.beginObject();
+
+        out.name("main");
+        out.value(value.getMain());
+
+        out.name("token");
+        out.value(value.getToken());
+
+        out.name("handlingOwnPresence");
+        out.value(value.isHandlingOwnPresence());
+
+        out.name("presenceSwitchDuration");
+        out.value(value.getPresenceDuration());
+
+        out.endObject();
+    }
+
+    @Override
+    public BotDescription read(JsonReader in) throws IOException {
+        String main = null;
+        String token = null;
+        boolean handlingOwnPresence = false;
+        long presenceDuration = 35;
+
+        in.beginObject();
+
+        while (in.hasNext()) {
+            if (in.peek().equals(JsonToken.END_OBJECT)) {
+                in.endObject();
+                break;
+            }
+
+            switch (in.nextName().toLowerCase(Locale.ROOT)) {
+                case "main":
+                    main = in.nextString();
+                    break;
+
+                case "token":
+                    token = in.nextString();
+                    break;
+
+                case "handlingownpresence":
+                    handlingOwnPresence = in.nextBoolean();
+                    break;
+
+                case "presenceswitchduration":
+                    presenceDuration = in.nextLong();
+                    break;
+
+                default:
+                    in.skipValue();
+                    break;
+            }
+        }
+
+        in.endObject();
+
+        if (main == null || token == null)
+            throw new IllegalArgumentException("Main and Token must be defined");
+
+        return new BotDescription(main, token, handlingOwnPresence, presenceDuration);
+    }
+}
---
+++ discord-botdriver/src/main/java/xyz/cronixzero/sapota/botdriver/BotDriver.java
@@ -0,0 +1,56 @@
+/* 
+Coded for Notazo
+Made by CronixZero
+Created 18.10.2021 - 21:06
+ */
+
+package xyz.cronixzero.sapota.botdriver;
+
+import net.dv8tion.jda.api.JDA;
+
+public abstract class BotDriver {
+
+    private BotDescription description;
+    private JDA bot;
+
+    /**
+     * This method is called while constructing the Bot
+     *
+     * @param bot The Bot Instance before final initialization
+     * @see JDA
+     */
+    public void onLoad(JDA bot) {
+    }
+
+    /**
+     * This method is called after constructing the bot
+     *
+     * @param bot The Bot Instance after final initialization
+     * @see JDA
+     */
+    public void onStart(JDA bot) {
+    }
+
+    /**
+     * @deprecated Only for internal uses
+     */
+    @Deprecated
+    public void setBot(JDA bot) {
+        if (this.bot != null)
+            throw new IllegalStateException("JDA Bot Instance can only be set once");
+
+        this.bot = bot;
+    }
+
+    public void setDescription(BotDescription description) {
+        this.description = description;
+    }
+
+    public JDA getBot() {
+        return bot;
+    }
+
+    public BotDescription getDescription() {
+        return description;
+    }
+}
