bdeb84fd9397eba7e83191fb303f7b7eddf05b37
CronixZero <CronixZero@users.noreply.github.com>
2021-11-10 19:28:25 +0100

    Add Discord Bot Presence

---
+++ discord-presence/src/main/java/de/cronixzero/notazo/presence/Presence.java
@@ -0,0 +1,59 @@
+/* 
+Coded for Sapota
+Made by CronixZero
+Created 15.10.2021 - 02:50
+ */
+
+package de.cronixzero.notazo.presence;
+
+import com.google.common.base.MoreObjects;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+
+public class Presence {
+
+    private final String text;
+    private final Activity.ActivityType type;
+    private final OnlineStatus status;
+
+    private String url;
+
+    public Presence(String text, Activity.ActivityType type, OnlineStatus status) {
+        this.text = text;
+        this.type = type;
+        this.status = status;
+    }
+
+    public Presence(String text, Activity.ActivityType type, OnlineStatus status, String url) {
+        this.text = text;
+        this.type = type;
+        this.status = status;
+        this.url = url;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Activity.ActivityType getType() {
+        return type;
+    }
+
+    public OnlineStatus getStatus() {
+        return status;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("text", text)
+                .add("type", type)
+                .add("status", status)
+                .add("url", url)
+                .toString();
+    }
+}
---
+++ discord-presence/src/main/java/de/cronixzero/notazo/presence/PresenceApi.java
@@ -0,0 +1,96 @@
+/* 
+Coded for Sapota
+Made by CronixZero
+Created 15.10.2021 - 03:14
+ */
+
+package de.cronixzero.notazo.presence;
+
+import com.google.common.flogger.FluentLogger;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Activity;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PresenceApi {
+
+    private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+    private final JDA bot;
+    private final String configPath;
+    private final List<Presence> presences = new ArrayList<>();
+
+    private int currentPresence;
+
+    public PresenceApi(JDA bot) {
+        this.bot = bot;
+        this.configPath = "presences.json";
+    }
+
+    public PresenceApi(JDA bot, String configPath) {
+        this.bot = bot;
+        this.configPath = configPath;
+    }
+
+    public void updatePresence() throws IOException {
+        considerUpdatingPresences();
+
+        if (presences.isEmpty())
+            throw new IllegalStateException("No Presences were added");
+
+        if (currentPresence >= presences.size())
+            currentPresence = 0;
+
+        Presence presence = presences.get(currentPresence);
+
+        if (presence.getUrl() == null)
+            bot.getPresence().setPresence(presence.getStatus(), Activity.of(presence.getType(), presence.getText()));
+        else
+            bot.getPresence().setPresence(presence.getStatus(), Activity.of(presence.getType(), presence.getText(), presence.getUrl()));
+
+        currentPresence++;
+    }
+
+    private void considerUpdatingPresences() throws IOException {
+        File configFile = new File(configPath);
+
+        if (!configFile.exists()) {
+            InputStream stream = getClass().getResourceAsStream(configPath);
+
+            if (stream == null) {
+                throw new NullPointerException(configPath + " does not exist");
+            }
+
+            Files.copy(stream, Paths.get("./"));
+
+            JsonArray configPresences;
+            try {
+                configPresences = new Gson().fromJson(new FileReader(configFile), JsonArray.class);
+            } catch (FileNotFoundException e) {
+                logger.atSevere().withCause(e).log("The Configuration File '" + configPath + "' could not be found");
+                return;
+            }
+
+            if (configPresences == null)
+                throw new IllegalStateException("The Configuration File '" + configPath + "' could not be read");
+
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            gsonBuilder.registerTypeAdapter(Presence.class, new PresenceTypeAdapter());
+            Gson gson = gsonBuilder.create();
+
+            presences.clear();
+
+            for (int i = 0; i < configPresences.size(); i++) {
+                Presence presence = gson.fromJson(configPresences.get(i), Presence.class);
+                presences.add(presence);
+            }
+        }
+    }
+}
